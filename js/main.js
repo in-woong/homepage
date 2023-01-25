a0c301ed44f6481fd313ccd24aad31bfd54fa96c
woong <inwoong100@gmail.com>
2023-01-25 23:37:35 +0900

    feat: set first scene layout

--- js/main.js
+++ js/main.js
@@ -14,26 +14,204 @@
       type: 'sticy',
       heightNum: 5,
       scrollHeight: 0,
-      objs: {},
-      values: {},
+      objs: {
+        container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+        canvas: document.querySelector('#video-canvas-0'),
+        context: document.querySelector('#video-canvas-0').getContext('2d'),
+        videoImages: [],
+      },
+      values: {
+        values: {
+          videoImageCount: 307,
+          imageSequence: [0, 306],
+          canvas_opacity: [1, 0, { start: 0.9, end: 1 }],
+          messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+          messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+          messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
+          messageD_opacity_in: [0, 1, { start: 0.7, end: 0.8 }],
+          messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+          messageB_translateY_in: [20, 0, { start: 0.3, end: 0.4 }],
+          messageC_translateY_in: [20, 0, { start: 0.5, end: 0.6 }],
+          messageD_translateY_in: [20, 0, { start: 0.7, end: 0.8 }],
+          messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+          messageB_opacity_out: [1, 0, { start: 0.45, end: 0.5 }],
+          messageC_opacity_out: [1, 0, { start: 0.65, end: 0.7 }],
+          messageD_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
+          messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
+          messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
+          messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
+          messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }],
+        },
+      },
     },
     {
       //1
       type: 'normal',
       scrollHeight: 0,
-      objs: {},
+      objs: {
+        container: document.querySelector('#scroll-section-1'),
+      },
     },
     {
       //2
       type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
-      objs: {},
+      objs: {
+        container: document.querySelector('#scroll-section-2'),
+      },
+      values: {},
+    },
+    {
+      //3
+      type: 'sticky',
+      heightNum: 5,
+      objs: {
+        container: document.querySelector('#scroll-section-3'),
+      },
       values: {},
     },
   ];
 
+  function scrollLoop() {
+    console.log('scrollLoop');
+    enterNewScene = false;
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i += 1) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (
+      delayedYOffset <
+      prevScrollHeight + sceneInfo[currentScene].scrollHeight
+    ) {
+      document.body.classList.remove('scroll-effect-end');
+    }
+
+    if (
+      delayedYOffset >
+      prevScrollHeight + sceneInfo[currentScene].scrollHeight
+    ) {
+      enterNewScene = true;
+      if (currentScene === sceneInfo.length - 1) {
+        document.body.classList.add('scroll-effect-end');
+      }
+
+      if (currentScene < sceneInfo.length - 1) {
+        currentScene += 1;
+      }
+
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if (delayedYOffset < prevScrollHeight) {
+      enterNewScene = true;
+      if (currentScene === 0) return;
+      currentScene -= 1;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if (enterNewScene) return;
+    // playAnimation();
+  }
+
+  function checkMenu() {
+    if (yOffset > 44) {
+      document.body.classList.add('local-nav-sticky');
+    } else {
+      document.body.classList.remove('local-nav-sticky');
+    }
+  }
+
+  function setLayout() {
+    for (let i = 0; i < sceneInfo.length; i += 1) {
+      if (sceneInfo[i].type === 'sticky') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      } else if (sceneInfo[i].type === 'normal') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+      }
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    let totalScrollHeight = 0;
+    yOffset = window.pageYOffset;
+    for (let i = 0; i < sceneInfo.length; i += 1) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
+
+    const heightRatio = window.innerHeight / 1080;
+    // sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
+    // sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
+  }
+
+  function setCanvasImages() {
+    //비디오와 이미지 사전준비
+    let imgElem;
+    for (let i = 0; i < sceneInfo[0].values.videoImageCount; i += 1) {
+      imgElem = new Image();
+      imgElem.src = `./video/file/${0001 + i}.jpg`;
+      sceneInfo[0].objs.videoImages.push(imgElem);
+    }
+  }
+
   window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('before-load');
+    setLayout();
+    sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+
+    let tempYOffset = yOffset;
+    let tempScrollCount = 0;
+
+    if (yOffset > 0) {
+      const siId = setInterval(() => {
+        window.scrollTo(0, tempYOffset);
+        tempYOffset += 5;
+        tempScrollCount += 1;
+        if (tempScrollCount > 20) clearInterval(siId);
+      }, 20);
+    }
+
+    window.addEventListener('scroll', () => {
+      yOffset = window.pageYOffset;
+      scrollLoop();
+      checkMenu();
+
+      // if (!rafState) {
+      //   rafId = requestAnimationFrame(loop);
+      //   rafState = true;
+      // }
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 900) {
+        window.location.reload();
+      }
+    });
+
+    window.addEventListener('orientationchange', () => {
+      scrollTo(0, 0);
+      setTimeout(() => {
+        wondow.location.reload();
+      }, 300);
+    });
+
+    document
+      .querySelector('.loading')
+      .addEventListener('transitionend', (e) => {
+        document.body.removeChild(e.currentTarget);
+      });
   });
+
+  setCanvasImages();
 })();
